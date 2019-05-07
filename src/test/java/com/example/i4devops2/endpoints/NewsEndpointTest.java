28ae0b1b15bc849981145b734a5a7fc1ed5bd9cc
BartvaiErika <erika.bartvai@gmail.com>
2019-05-07 14:32:01 +0200

    afternoon commit circleci2

--- src/test/java/com/example/i4devops2/endpoints/NewsEndpointTest.java
+++ src/test/java/com/example/i4devops2/endpoints/NewsEndpointTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-class MessageEndpointTest {
+class NewsEndpointTest {
 
   @Autowired
   TestRestTemplate restTemplate;
